53370fb383790f7b864f7386fcb5cf23e7e0e6d1
nadiaz2 <nadiaz2@wpi.edu>
2023-09-30 21:58:25 -0400

    Create simple canvas

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	const canvas = document.getElementById("myCanvas");
+	const ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#92B901";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
