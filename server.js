67badaf2876e649a02e9d1ac19139ed21d549021
SmeeBoi <sameer.desai.boston@gmail.com>
2023-10-03 23:39:17 -0400

    need simple express server for requirement

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public')) // Static files from public directory
+app.use(express.json()) // For parsing application/json
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
